7ed2b8801b532be35e6cd28e36ec31873771dfec
Jason Yeomans <jyeomans@perfectsensedigital.com>
2015-07-30 11:19:11 -0400

    changes

--- js/jq.js
+++ js/jq.js
@@ -20,127 +20,127 @@ function getTable(building) {
 
 
 $(document).ready(function() {
-var addToCount = 0;
+  var addToCount = 0;
 
-$('.menu-item div').click(function() {
-  var i = $(this).parent().prevAll().length;
-  console.log(i + "menu index");
-  if (!($('#class' + i).hasClass('expandnav'))) {
-    $('#class' + i).addClass('expandnav');
-    $('#class' + i + ' ul li').each(function(index) {
-      console.log(index + " li");
-      $(this).delay((index + 1) * 50).queue(function(next) {
-        $(this).addClass("slideinleft");
-        next();
-      });;
-    });
-  } else {
-    $('#class' + i).removeClass('expandnav');
-    $('#class' + i + ' ul li').removeClass('slideinleft');
-  }
-});
+  $('.menu-item div').click(function() {
+    var i = $(this).parent().prevAll().length;
+    console.log(i + "menu index");
+    if (!($('#class' + i).hasClass('expandnav'))) {
+      $('#class' + i).addClass('expandnav');
+      $('#class' + i + ' ul li').each(function(index) {
+        console.log(index + " li");
+        $(this).delay((index + 1) * 50).queue(function(next) {
+          $(this).addClass("slideinleft");
+          next();
+        });;
+      });
+    } else {
+      $('#class' + i).removeClass('expandnav');
+      $('#class' + i + ' ul li').removeClass('slideinleft');
+    }
+  });
 
-$('#homebtn').click(function() {
-  $('#featureform').hide();
-  $('#bugform').hide();
-  $('#table').hide();
-  $('#faqpage').hide();
-  $('#homepage').show();
-});
+  $('#homebtn').click(function() {
+    $('#featureform').hide();
+    $('#bugform').hide();
+    $('#table').hide();
+    $('#faqpage').hide();
+    $('#homepage').show();
+  });
 
-$('#faqbtn').click(function() {
-  $('#featureform').hide();
-  $('#bugform').hide();
-  $('#table').hide();
-  $('#homepage').hide();
-  $('#faqpage').show();
-  $('#faqpage').customScrollbar();
-});
+  $('#faqbtn').click(function() {
+    $('#featureform').hide();
+    $('#bugform').hide();
+    $('#table').hide();
+    $('#homepage').hide();
+    $('#faqpage').show();
+    $('#faqpage').customScrollbar();
+  });
 
-$('.menu-item ul li').click(function() {
-  console.log($(this).html());
-  switch ($(this).html()) {
-    case "The Complex":
-      getTable("The_Complex");
-      break;
-    case "Brunswick":
-      getTable("Brunswick");
-      break;
-    case "Carroll":
-      getTable("Carroll");
-      break;
-    case "Dickenson":
-      getTable("Dickenson");
-      break;
-    case "Essex":
-      getTable("Essex");
-      break;
-    case "Franklin":
-      getTable("Franklin");
-      break;
-    case "Grayson":
-      getTable("Grayson");
-      break;
-    case "Adams":
-      getTable("Adams");
-      break;
-    case "Harrison":
-      getTable("Harrison");
-      break;
-    case "Jackson":
-      getTable("Jackson");
-      break;
-    case "Jefferson":
-      getTable("Jefferson");
-      break;
-    case "Kennedy":
-      getTable("Kennedy");
-      break;
-    case "Lincoln":
-      getTable("Lincoln");
-      break;
-    case "Madison":
-      getTable("Madison");
-      break;
-    case "Monroe":
-      getTable("Monroe");
-      break;
-    case "Roosevelt":
-      getTable("Roosevelt");
-      break;
-    case "Truman":
-      getTable("Truman");
-      break;
-    case "Washington":
-      getTable("Washington");
-      break;
-    case "Wilson":
-      getTable("Wilson");
-      break;
-    case "Commonwealth":
-      getTable("Commonwealth");
-      break;
-    case "Dominion":
-      getTable("Dominion");
-      break;
+  $('.menu-item ul li').click(function() {
+    console.log($(this).html());
+    switch ($(this).html()) {
+      case "The Complex":
+        getTable("The_Complex");
+        break;
+      case "Brunswick":
+        getTable("Brunswick");
+        break;
+      case "Carroll":
+        getTable("Carroll");
+        break;
+      case "Dickenson":
+        getTable("Dickenson");
+        break;
+      case "Essex":
+        getTable("Essex");
+        break;
+      case "Franklin":
+        getTable("Franklin");
+        break;
+      case "Grayson":
+        getTable("Grayson");
+        break;
+      case "Adams":
+        getTable("Adams");
+        break;
+      case "Harrison":
+        getTable("Harrison");
+        break;
+      case "Jackson":
+        getTable("Jackson");
+        break;
+      case "Jefferson":
+        getTable("Jefferson");
+        break;
+      case "Kennedy":
+        getTable("Kennedy");
+        break;
+      case "Lincoln":
+        getTable("Lincoln");
+        break;
+      case "Madison":
+        getTable("Madison");
+        break;
+      case "Monroe":
+        getTable("Monroe");
+        break;
+      case "Roosevelt":
+        getTable("Roosevelt");
+        break;
+      case "Truman":
+        getTable("Truman");
+        break;
+      case "Washington":
+        getTable("Washington");
+        break;
+      case "Wilson":
+        getTable("Wilson");
+        break;
+      case "Commonwealth":
+        getTable("Commonwealth");
+        break;
+      case "Dominion":
+        getTable("Dominion");
+        break;
 
-    case "Submit a Bug":
-      $('#homepage').hide();
-      $('#table').hide();
-      $('#featureform').hide();
-      $('#bugform').show();
-      break;
-    case "Request a Feature":
-      $('#homepage').hide();
-      $('#table').hide();
-      $('#bugform').hide();
-      $('#featureform').show();
-      break;
-  }
-});
+      case "Submit a Bug":
+        $('#homepage').hide();
+        $('#table').hide();
+        $('#featureform').hide();
+        $('#bugform').show();
+        break;
+      case "Request a Feature":
+        $('#homepage').hide();
+        $('#table').hide();
+        $('#bugform').hide();
+        $('#featureform').show();
+        break;
+    }
+  });
 
 
-$("#add2Room").click(function() {
+  $("#add2Room").click(function() {
     $('#addtip').hide();
     console.log(addToCount);
     if (addToCount < 1) {
@@ -149,38 +149,40 @@ $("#add2Room").click(function() {
       newTextBoxDiv.after().html('<form action="<?php echo $_SERVER[\'PHP_SELF\']; ?>" method="post"> <input type="text" name="nameTextbox" id="nameTextbox" placeholder="Name" style="width:30%" size="25" maxlength="40"> <select name="bedTextbox" id="bedTextbox" placeholder="Bed Letter" style="width:12%"><option>Bed</option><option>A</option><option>B</option><option>C</option><option>D</option></select> <select name="buildingTextbox" id="buildingTextbox" style="width:20%"><option>Building</option><option>The Complex</option><option>Brunswick</option><option>Carroll</option><option>Dickenson</option><option>Essex</option><option>Franklin</option><option>Grayson</option><option>Adams</option><option>Harrison</option><option>Jackson</option><option>Jefferson</option><option>Kennedy</option><option>Lincoln</option><option>Madison</option><option>Monroe</option><option>Roosevelt</option><option>Truman</option><option>Washington</option><option>Wilson</option><option>Dominion</option><option>Commonwealth</option></select><select name="floorTextbox" id="floorTextbox" style="width:12%"><option>1</option><option>2</option><option>3</option></select></form>');
       newTextBoxDiv.appendTo("#head");
 
-    }); $("#add2Room").css({
-    "background": "url('images/check-alt.png') center top no-repeat"
-  }); $("#add2Room").addClass("wiggleanim"); addToCount++;
-} else {
-  console.log("Else");
-  var name = $("input[id$=nameTextbox]").val(),
-    floor = $("select[id$=floorTextbox]").val(),
-    building = $("select[id$=buildingTextbox]").val(),
+      $("#add2Room").css({
+        "background": "url('images/check-alt.png') center top no-repeat"
+      });
+      $("#add2Room").addClass("wiggleanim");
+      addToCount++;
+    } else {
+      console.log("Else");
+      var name = $("input[id$=nameTextbox]").val(),
+        floor = $("select[id$=floorTextbox]").val(),
+        building = $("select[id$=buildingTextbox]").val(),
 
-    $("#AddToRoomDiv").remove();
-  addToCount--;
-  $("#add2Room").css({
-    "background": ""
-  });
-  $("#add2Room").removeClass("wiggleanim");
-  if ((name.length > 0) && (floor != "floor") && (building != "Building") && (name.toLowerCase().indexOf("penis") < 0) && (name.toLowerCase().indexOf("ass") < 0) && (name.toLowerCase().indexOf("yeomans") < 0) && (name.toLowerCase().indexOf("fuck") < 0) && (name.toLowerCase().indexOf("bitch") < 0) && (name.toLowerCase().indexOf("fag") < 0) && (name.toLowerCase().indexOf("faggot") < 0) && (name.toLowerCase().indexOf("asshole") < 0) && (name.toLowerCase().indexOf("nigga") < 0) && (name.toLowerCase().indexOf("nigger") < 0)) {
-    //if(name.length > 0) {
-    console.log("Ajaxing");
-    $.ajax({
-      url: 'php/queries.php',
-      data: {
-        action: 'addToFloor',
-        name: name,
-        floor: floor,
-        building: building
-      },
-      type: 'post',
-      success: function(output) {
-        getTable(building);
+        $("#AddToRoomDiv").remove();
+      addToCount--;
+      $("#add2Room").css({
+        "background": ""
+      });
+      $("#add2Room").removeClass("wiggleanim");
+      if ((name.length > 0) && (floor != "floor") && (building != "Building") && (name.toLowerCase().indexOf("penis") < 0) && (name.toLowerCase().indexOf("ass") < 0) && (name.toLowerCase().indexOf("yeomans") < 0) && (name.toLowerCase().indexOf("fuck") < 0) && (name.toLowerCase().indexOf("bitch") < 0) && (name.toLowerCase().indexOf("fag") < 0) && (name.toLowerCase().indexOf("faggot") < 0) && (name.toLowerCase().indexOf("asshole") < 0) && (name.toLowerCase().indexOf("nigga") < 0) && (name.toLowerCase().indexOf("nigger") < 0)) {
+        //if(name.length > 0) {
+        console.log("Ajaxing");
+        $.ajax({
+          url: 'php/queries.php',
+          data: {
+            action: 'addToFloor',
+            name: name,
+            floor: floor,
+            building: building
+          },
+          type: 'post',
+          success: function(output) {
+            getTable(building);
+          }
+        });
       }
-    });
-  }
-}
-});
+    }
+  });
 });
